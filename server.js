305fd5bcddaa10afc65399ba0b1f5c0a49ac521e
Denys Romanyshyn <DenysYkr135@gmail.com>
2025-12-15 23:15:28 +0200

    Add Server

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://admin:12Sm8O43@athelon.n4ntkjl.mongodb.net/AthelonDB")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err))
+
+const productSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  description: String,
+  team: String,
+  sizes: [String],
+  oldPrice: Number,
+  newPrice: Number,
+  rating: Number,
+  category: String,
+  brand: String,
+  images: [String],
+  inStock: Number,
+  reviews: Array
+})
+
+const Product = mongoose.model("Product", productSchema)
+
+app.get("/", (req, res) => {
+  res.send("Server is running")
+})
+
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find()
+  res.json(products)
+})
+
+const PORT = process.env.PORT || 5001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
